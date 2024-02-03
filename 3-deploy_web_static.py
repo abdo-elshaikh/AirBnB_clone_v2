80a5e6105980207f60fa82cd52b1a28a14f1d9b8
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-02-04 00:48:37 +0200

    update do deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,67 +1,80 @@
-#!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
-import os
-from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+#!/usr/bin/env python3
+"""
+Fabric script that creates and distributes an archive to your web servers
+"""
 
+from fabric.api import env, local, run, put
+from datetime import datetime
+from os.path import exists
 
 env.hosts = ["54.175.223.125", "54.196.34.67"]
-"""The list of host server IP addresses."""
 
-
-@runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
+    """
+    Function to pack the web_static content into a .tgz file
+    """
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+        now = datetime.now()
+        time_format = now.strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(time_format)
+
+        local("mkdir -p versions")
+        local("tar -czvf {} web_static".format(archive_path))
+
+        return archive_path
+    except Exception as e:
+        return None
 
 
 def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path to the archived static files.
     """
-    if not os.path.exists(archive_path):
+    Function to deploy an archive to the web servers
+    """
+    if not exists(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
+        # Upload archive
+        put(archive_path, "/tmp/")
+
+        # Create release folder
+        release_folder = "/data/web_static/releases/{}".format(
+            archive_path.split("/")[-1].split(".")[0]
+        )
+        run("mkdir -p {}".format(release_folder))
+
+        # Unpack archive
+        run("tar -xzf /tmp/{} -C {}".format(archive_path.split("/")[-1], release_folder))
+
+        # Remove uploaded archive
+        run("rm /tmp/{}".format(archive_path.split("/")[-1]))
+
+        # Move contents to release folder
+        run("mv {}/web_static/* {}".format(release_folder, release_folder))
+
+        # Remove symbolic link
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version deployed!')
-        success = True
-    except Exception:
-        success = False
-    return success
+
+        # Create new symbolic link
+        run("ln -s {} /data/web_static/current".format(release_folder))
+
+        print("New version deployed!")
+        return True
+    except Exception as e:
+        return False
 
 
 def deploy():
-    """Archives and deploys the static files to the host servers.
+    """
+    Function to deploy the latest version of the web_static folder
     """
     archive_path = do_pack()
-    return do_deploy(archive_path) if archive_path else False
+
+    if not archive_path:
+        return False
+
+    return do_deploy(archive_path)
+
+
+if __name__ == "__main__":
+    deploy()
