8e226cfeb556d3d236628011860bcb4bc9625cc7
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-02-04 17:20:53 +0200

    update task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,34 +1,17 @@
-#!/bin/bash
-
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null
-then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
-
-# Create necessary directories
-sudo mkdir -p /data/web_static/{releases/test,shared}
-
-# Create a fake HTML file for testing
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-# Create or recreate symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-nginx_config="location /hbnb_static {\n    alias /data/web_static/current/;\n}"
-sudo sed -i "/server_name _;/a $nginx_config" /etc/nginx/sites-available/default
-
-# Restart Nginx and check if it restarts successfully
-sudo service nginx restart
-if [ $? -ne 0 ]; then
-    echo "Nginx restart failed"
-    exit 1
-fi
-
-# Exit successfully
-exit 0
+#!/usr/bin/env bash
+# Prepare web server for deployment
+apt-get -y update
+apt-get -y install nginx
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "61i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+service nginx restart
