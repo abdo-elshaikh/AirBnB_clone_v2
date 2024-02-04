7afd280bbdd790463f97b53e86da6deed635e985
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-02-04 20:27:33 +0200

    updates

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,83 +1,61 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+Distributes an archive to my web servers,
+using the function deploy
 """
-
-from fabric.api import env, run, put, local
-from os.path import exists
+from fabric.api import *
 from datetime import datetime
+import os
 
 env.hosts = ['54.175.223.125', '54.196.34.67']
 env.user = 'ubuntu'
 
 
+def deploy():
+    ''' Deploys archive '''
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
+
+
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static
-    """
+    '''
+    Generates a tgz archive from the
+    contents of the web_static folder
+    '''
     try:
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_path = "versions/web_static_{}.tgz".format(timestamp)
-        local("mkdir -p versions")
-        local("tar -cvzf {} web_static".format(archive_path))
+        local('mkdir -p versions')
+        datetime_format = '%Y%m%d%H%M%S'
+        archive_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(datetime_format))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        print('web_static packed: {} -> {}'.format(archive_path,
+              os.path.getsize(archive_path)))
         return archive_path
-    except Exception as e:
-        print("Packaging failed: {}".format(e))
+    except:
         return None
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers using the function do_deploy
-
-    archive_path: path of the archive
-    """
-    if not exists(archive_path):
+    '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
         return False
-
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
     try:
-        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, '/tmp/')
-
-        # Get the base name of the archive without the extension
-        archive_filename = archive_path.split('/')[-1].split('.')[0]
-
-        # Create the destination directory for the uncompressed archive
-        run('mkdir -p /data/web_static/releases/{}'
-            .format(archive_filename))
-
-        # Uncompress the archive to the destination directory
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
-            .format(archive_filename, archive_filename))
-
-        # Remove the archive from the web server
-        run('rm /tmp/{}.tgz'.format(archive_filename))
-
-        # Move the contents of the uncompressed archive to the web server
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'
-            .format(archive_filename, archive_filename))
-
-        # Remove the web_static directory inside the destination directory
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(archive_filename))
-
-        # Remove the current symbolic link
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
         run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link linked to the new version of the code
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(archive_filename))
-
-        print("New version deployed!")
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
         return True
-
-    except Exception as e:
-        print("Deployment failed: {}".format(e))
+    except:
         return False
-
-
-def deploy():
-    """ Archives and deploys the static files to the host servers. """
-    archive_path = do_pack()
-    return do_deploy(archive_path) if archive_path else False
