555fd27269df87c532d1709bc14338bdcc3783a4
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-02-04 17:26:22 +0200

    update deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,83 +1,61 @@
 #!/usr/bin/python3
 """
-Fabric script that creates and distributes an archive to your web servers
+Distributes an archive to my web servers,
+using the function deploy
 """
-
-from fabric.api import env, local, run, put
+from fabric.api import *
 from datetime import datetime
-from os.path import exists
+import os
 
-env.hosts = ["54.175.223.125", "54.196.34.67"]
-""" The list of host server IP addresses. """
+env.hosts = ['54.175.223.125', '54.196.34.67']
+env.user = 'ubuntu'
+
+
+def deploy():
+    ''' Deploys archive '''
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
 
 
 def do_pack():
-    """
-    Function to pack the web_static content into a .tgz file
-    """
+    '''
+    Generates a tgz archive from the
+    contents of the web_static folder
+    '''
     try:
-        now = datetime.now()
-        time_format = now.strftime("%Y%m%d%H%M%S")
-        archive_path = "versions/web_static_{}.tgz".format(time_format)
-
-        local("mkdir -p versions")
-        local("tar -czvf {} web_static".format(archive_path))
-
+        local('mkdir -p versions')
+        datetime_format = '%Y%m%d%H%M%S'
+        archive_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(datetime_format))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        print('web_static packed: {} -> {}'.format(archive_path,
+              os.path.getsize(archive_path)))
         return archive_path
-    except Exception as e:
+    except:
         return None
 
 
 def do_deploy(archive_path):
-    """
-    Function to deploy an archive to the web servers
-    """
-    if not exists(archive_path):
+    '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
         return False
-
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
     try:
-        # Upload archive
-        put(archive_path, "/tmp/")
-
-        # Create release folder
-        release_folder = "/data/web_static/releases/{}".format(
-            archive_path.split("/")[-1].split(".")[0]
-        )
-        run("mkdir -p {}".format(release_folder))
-
-        # Unpack archive
-        run("tar -xzf /tmp/{} -C {}"
-            .format(archive_path.split("/")[-1], release_folder))
-
-        # Remove uploaded archive
-        run("rm /tmp/{}".format(archive_path.split("/")[-1]))
-
-        # Move contents to release folder
-        run("mv {}/web_static/* {}".format(release_folder, release_folder))
-
-        # Remove symbolic link
-        run("rm -rf /data/web_static/current")
-
-        # Create new symbolic link
-        run("ln -s {} /data/web_static/current".format(release_folder))
-
-        print("New version deployed!")
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
         return True
-    except Exception as e:
+    except:
         return False
-
-
-def deploy():
-    """
-    Function to deploy the latest version of the web_static folder
-    """
-    archive_path = do_pack()
-
-    if not archive_path:
-        return False
-
-    return do_deploy(archive_path)
-
-
-if __name__ == "__main__":
-    deploy()
