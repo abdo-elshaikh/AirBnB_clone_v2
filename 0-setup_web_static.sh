eb0660a1b149ddbc4fbde37f066508e50716d178
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-02-04 17:19:20 +0200

    update task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Install Nginx if not already installed
-if ! command -v nginx &>/dev/null; then
-  sudo apt-get update
-  sudo apt-get -y install nginx
+if ! command -v nginx &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get -y install nginx
 fi
 
 # Create necessary directories
 sudo mkdir -p /data/web_static/{releases/test,shared}
 
 # Create a fake HTML file for testing
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html >/dev/null
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 # Create or recreate symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -22,8 +23,12 @@ sudo chown -R ubuntu:ubuntu /data/
 nginx_config="location /hbnb_static {\n    alias /data/web_static/current/;\n}"
 sudo sed -i "/server_name _;/a $nginx_config" /etc/nginx/sites-available/default
 
-# Restart Nginx
+# Restart Nginx and check if it restarts successfully
 sudo service nginx restart
+if [ $? -ne 0 ]; then
+    echo "Nginx restart failed"
+    exit 1
+fi
 
 # Exit successfully
 exit 0
